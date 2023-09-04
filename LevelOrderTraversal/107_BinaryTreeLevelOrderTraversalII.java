e3916055b67ec37eb73cd2b3f22c78b41fa65c9a
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-09-05 00:15:48 +0530

    Level Order Traversal | Bottom to Top | BFS

---
+++ LevelOrderTraversal/107_BinaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,44 @@
+//bottom to top
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while(!q.isEmpty()){
+            int sz = q.size();
+            List<Integer> curr = new ArrayList<>(sz);
+            for(int i=0; i<sz; i++){
+                TreeNode node = q.poll();
+                curr.add(node.val);
+
+                if(node.left != null){
+                    q.offer(node.left);
+                }
+                if(node.right != null){
+                    q.offer(node.right);
+                }
+            }
+            ans.add(0, curr);
+        }
+        return ans;
+    }
+}
