a2682fafa97764afce226afb50e0978152f03b5d
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-09-16 18:22:12 +0530

    Linked List | Binary Tree

---
+++ DFS/114_FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null)){
+            return;
+        }
+
+        if(root.left != null){
+            flatten(root.left);
+
+            TreeNode tmp = root.right;
+            root.right = root.left;
+            root.left = null;
+
+            TreeNode t = root.right;
+            while(t.right != null){
+                t = t.right;
+            }
+            t.right = tmp;
+        }
+        flatten(root.right);
+    }
+}
